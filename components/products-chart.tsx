390f977fbc89a26cdf4d029b94f4b32d93c5a8fc
Nader PJR <pjrnader@gmail.com>
2025-12-13 21:59:06 +0430

    weekly products

---
+++ components/products-chart.tsx
@@ -0,0 +1,9 @@
+interface ChartData {
+    week: string;
+    products: number;
+}
+
+export default function ProductsChart({ data }: { data: ChartData[] }) {
+    console.log(data);
+    return <div></div>
+}
