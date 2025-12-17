dc13b469746dd53edeccdeaa4b1d3d05de9b9339
Nader PJR <pjrnader@gmail.com>
2025-12-17 20:47:13 +0430

    inventory list created

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,6 +1,14 @@
 import Sidebar from "@/components/Sidebar";
+import { deleteProduct } from "@/lib/actions/products";
+import { getCurrentUser } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 
 export default async function InventoryPage({ }) {
+
+    const user = await getCurrentUser();
+    const userId = user.id;
+    const totalProducts = await prisma.product.findMany({ where: { userId } });
+
     return (
         <div className="min-h-screen bg-gray-50">
             <Sidebar currentPath="/inventory" />
@@ -33,7 +41,24 @@ export default async function InventoryPage({ }) {
                             </thead>
 
                             <tbody className="bg-white divide-y divide-gray-200">
-
+                                {totalProducts.map((product, key) => (
+                                    <tr key={key} className="hover:bg-gray-50">
+                                        <td className="px-6 py-4 text-sm text-gray-500">{product.name}</td>
+                                        <td className="px-6 py-4 text-sm text-gray-500">{product.sku || "-"}</td>
+                                        <td className="px-6 py-4 text-sm text-gray-500">${Number(product.price).toFixed(2)}</td>
+                                        <td className="px-6 py-4 text-sm text-gray-500">{product.quantity}</td>
+                                        <td className="px-6 py-4 text-sm text-gray-500">{product.lowStockAt || "="}</td>
+                                        <td className="px-6 py-4 text-sm text-gray-500">
+                                            <form action={async (formData: FormData) => {
+                                                "use server";
+                                                await deleteProduct(formData);
+                                            }}>
+                                                <input type="hidden" name="id" value={product.id} />
+                                                <button className="text-red-600 hover:text-red-900">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
