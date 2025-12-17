e70d9e407fdbbd93ea7a0f1e0e686fcfa7cbfa1f
Nader PJR <pjrnader@gmail.com>
2025-12-17 21:06:13 +0430

    search bar created

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -3,11 +3,15 @@ import { deleteProduct } from "@/lib/actions/products";
 import { getCurrentUser } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 
-export default async function InventoryPage({ }) {
+export default async function InventoryPage({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
 
     const user = await getCurrentUser();
     const userId = user.id;
-    const totalProducts = await prisma.product.findMany({ where: { userId } });
+
+    const params = await searchParams
+    const q = (params.q ?? "").trim();
+
+    const totalProducts = await prisma.product.findMany({ where: { userId, name: { contains: q, mode: "insensitive" } } });
 
     return (
         <div className="min-h-screen bg-gray-50">
@@ -23,6 +27,14 @@ export default async function InventoryPage({ }) {
                 </div>
 
                 <div className="space-y-6">
+
+                    {/* Search  */}
+                    <div className="bg-white rounded-lg border border-gray-200 p-6">
+                        <form className="flex gap-2" action="/inventory" method="GET">
+                            <input name="q" placeholder="Search products..." className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent" />
+                            <button className="px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700">Search</button>
+                        </form>
+                    </div>
 
 
 
