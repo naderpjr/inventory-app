79ae22ddc08e8752b4fdd37dd19f3bea5a74aa04
Nader PJR <pjrnader@gmail.com>
2025-12-17 22:33:34 +0430

    pagination logic

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Pagination from "@/components/pagination";
 import Sidebar from "@/components/Sidebar";
 import { deleteProduct } from "@/lib/actions/products";
--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,3 +1,5 @@
+
+
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
 
@@ -11,7 +13,38 @@ interface PaginationProps {
 export default function Pagination({ currentPage, totalPages, baseUrl, searchParams }: PaginationProps) {
     if (totalPages <= 1) return null;
 
-    
+    const getPageUrl = (page: number) => {
+        const params = new URLSearchParams({ ...searchParams, page: String(page) });
+        return `${baseUrl}?${params.toString()}`
+    }
+
+    const getVisiblePages = () => {
+        const delta = 2;
+        const range = [];
+        const rangeWithDots = [];
+
+        for (let i = Math.max(2, currentPage - delta); i <= Math.min(totalPages - 1, currentPage + delta); i++) {
+            range.push(i);
+        }
+
+        if (currentPage - delta > 2) {
+            rangeWithDots.push(1, "...");
+        } else {
+            rangeWithDots.push(1);
+        }
+
+        rangeWithDots.push(...range)
+
+        if (currentPage + delta < totalPages - 1) {
+            rangeWithDots.push("...", totalPages);
+        } else {
+            rangeWithDots.push(totalPages);
+        }
+
+        return rangeWithDots;
+    }
+
+    const visiblePages = getVisiblePages();
 
     return <nav className="flex items-center justify-center gap-1">
         <Link href="" className={`flex items-center px-3 py-2 text-sm font-medium rounded-lg ${currentPage <= 1 ?
@@ -19,7 +52,24 @@ export default function Pagination({ currentPage, totalPages, baseUrl, searchPar
             "text-gray-700 hover:bg-gray-100 bg-white border border-gray-300"}`} aria-disabled={currentPage <= 1}>
             <ChevronLeft /> Previous
         </Link>
-        <Link href="" className={`flex items-center px-3 py-2 text-sm font-medium rounded-lg ${currentPage >= totalPages ?
+
+        {visiblePages.map((page, key) => {
+
+            if (page === "...") {
+                return (
+                    <span key={key} className="px-3 py-2 text-sm text-gray-500">
+                        ...
+                    </span>
+                )
+            }
+
+            const pageNumber = page as number;
+            const isCurrentPage = pageNumber === currentPage;
+
+            return <Link key={key} href={"/"} className={`px-3 py-2 text-sm font-medium rounded-lg ${isCurrentPage ? "bg-purple-600 text-white" : "text-gray-700 hover:bg-gray-100 bg-white border border-gray-300"}`}>{pageNumber}</Link>
+        })}
+
+        <Link href={getPageUrl(currentPage + 1)} className={`flex items-center px-3 py-2 text-sm font-medium rounded-lg ${currentPage >= totalPages ?
             "text-gray-400 cursor-not-allowed bg-gray-100" :
             "text-gray-700 hover:bg-gray-100 bg-white border border-gray-300"}`} aria-disabled={currentPage >= totalPages}>
             Next
