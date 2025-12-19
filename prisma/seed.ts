89a80bd001dc792e5718e5632c0bcba6d8851b23
Nader PJR <pjrnader@gmail.com>
2025-12-19 21:18:59 +0430

    seed.ts changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,31 @@
-import { PrismaClient } from "../app/generated/prisma/client"; 
-import "dotenv/config";
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    const demoUserId = "5f4ea70c-e947-4357-83ca-0e4a5ed9d634";
+  const demoUserId = '5f4ea70c-e947-4357-83ca-0e4a5ed9d634';
 
-    await prisma.product.createMany({
-        data: Array.from({ length: 25}).map((_, i) => ({
-            userId: demoUserId,
-            name: `Product ${i + 1}`,
-            price: (Math.random() * 90 + 10).toFixed(2),
-            quantity: Math.floor(Math.random() * 20),
-            lowStockAt: 5,
-            createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
-        })),
-    })
+  await prisma.product.createMany({
+    data: Array.from({ length: 25 }).map((_, i) => ({
+      userId: demoUserId,
+      name: `Product ${i + 1}`,
+      price: Number((Math.random() * 90 + 10).toFixed(2)),
+      quantity: Math.floor(Math.random() * 20),
+      lowStockAt: 5,
+      createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+    })),
+  });
 
-    console.log("Seed data created successfully.");
-    console.log(`Created 25 products for user ID: ${demoUserId}`);
-
+  console.log('Seed data created successfully.');
+  console.log(`Created 25 products for user ID: ${demoUserId}`);
 }
 
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    }); 
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
